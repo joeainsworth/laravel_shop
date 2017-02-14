2a345f4eb22de7dca293ccd246988e8c03860f3f
Joe Ainsworth <joeainsworth@Joes-iMac.lan>
2017-02-14 20:15:14 +0000

    Cart now works with Buyable interface on Product model, much improved. Added form-inline

--- resources/views/product/partials/add.blade.php
+++ resources/views/product/partials/add.blade.php
@@ -1,4 +1,4 @@
-{{ Form::open(['route' => 'cart.index', 'method' => 'post']) }}
+{{ Form::open(['route' => 'cart.index', 'method' => 'post', 'class' => 'form-inline']) }}
 	<input type="hidden" name="id" value="{{ $product->id }}">
 	<input type="hidden" name="name" value="{{ $product->name }}">
 	<input type="hidden" name="price" value="{{ $product->price }}">
